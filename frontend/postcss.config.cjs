df7d84b3515cb0d8e9bfbcc94c078d548369132f
gautam dogra <gdogra@gmail.com>
2025-04-04 16:54:00 -0700

    fix(postcss): fallback to postcss.config.js in CommonJS syntax

--- frontend/postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-// frontend/postcss.config.cjs
-
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-};
-
---
+++ frontend/postcss.config.js
@@ -0,0 +1,10 @@
+// frontend/postcss.config.js
+
+/** @type {import('postcss').ProcessOptions} */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
