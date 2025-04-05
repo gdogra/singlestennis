3dee7a78dcd714c5fb3ed0a2b24bb2b06f8a8f14
gautam dogra <gdogra@gmail.com>
2025-04-05 11:41:38 -0700

    feat: add MatchOverview component to fix build

---
+++ frontend/src/components/admin/MatchOverview.jsx
@@ -0,0 +1,9 @@
+// src/components/admin/MatchOverview.jsx
+import React from 'react';
+
+const MatchOverview = () => {
+  return <div>Match Overview Placeholder</div>;
+};
+
+export default MatchOverview;
+
