df3439583207d4e5897c191544738859619247b6
gautam dogra <gdogra@gmail.com>
2025-04-07 10:40:03 -0700

    Add db/index.js for PostgreSQL connection

---
+++ backend/db/index.js
@@ -0,0 +1,14 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+});
+
+export default pool;
+
