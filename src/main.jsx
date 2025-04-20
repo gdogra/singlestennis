17331f1c7220c9f2b0fd89e730808d6cd5ceedfa
gautam dogra <gdogra@gmail.com>
2025-04-20 13:42:07 -0700

    Fix router nesting issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,11 @@
-// src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-
-import App from './App.jsx'
-import './index.css'
-import './App.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css'; // optional, if you have global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Wrap your whole app in a Router so useNavigate, useLocation, etc. work */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
-)
+);
 
