a24e8b6fdd63d94598df9acf1bb982925fecd8f5
gautam dogra <gdogra@gmail.com>
2025-04-21 15:40:49 -0700

    fix: remove nested Router, consolidate <BrowserRouter> in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-// src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Routes, Route, NavLink } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import PlayerRankings from './pages/PlayerRankings';
-import Dashboard from './pages/Dashboard';
-import ChallengesPage from './pages/Challenges';
-import CalendarPage from './pages/Calendar';
+import HomePage from './pages/Home.jsx';
+import PlayerRankings from './pages/PlayerRankings.jsx';
+import ProfilePage from './pages/Profile.jsx';
+import ChallengesPage from './pages/Challenges.jsx';
+import CalendarPage from './pages/Calendar.jsx';
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <Navbar />
-      <div className="mt-16">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/:id" element={<Profile />} />
-          <Route path="/leaderboard" element={<PlayerRankings />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/challenges" element={<ChallengesPage />} />
-          <Route path="/calendar" element={<CalendarPage />} />
-        </Routes>
-      </div>
-      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
-    </Router>
+    <>
+      <nav className="p-6 bg-white shadow">
+        <NavLink to="/" className="mr-6 hover:underline">SingleTennis</NavLink>
+        <NavLink to="/leaderboard" className="mr-6 hover:underline">Leaderboard</NavLink>
+        <NavLink to="/profile" className="mr-6 hover:underline">Profile</NavLink>
+        <NavLink to="/challenges" className="mr-6 hover:underline">Challenges</NavLink>
+        <NavLink to="/calendar" className="hover:underline">Calendar</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="leaderboard" element={<PlayerRankings />} />
+        <Route path="profile/:id?" element={<ProfilePage />} />
+        <Route path="challenges" element={<ChallengesPage />} />
+        <Route path="calendar" element={<CalendarPage />} />
+      </Routes>
+    </>
   );
 }
 
-export default App;
