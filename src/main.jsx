3c789b6d7d04db10eb265dc0ac38d6c5500f0de0
gautam dogra <gdogra@gmail.com>
2025-04-21 12:01:18 -0700

    chore: complete Tailwind CSS + PostCSS setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 // src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'              // ← make sure this import is here
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';          // ← pull in Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
 
