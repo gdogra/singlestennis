2cf24ed2d0ce762b5211d65ea343f749d517acea
gautam dogra <gdogra@gmail.com>
2025-04-21 11:47:11 -0700

    fix: wire up Tailwind CSS (add index.css import & config)

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css'; // optional, if you have global styles
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'              // ← make sure this import is here
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
